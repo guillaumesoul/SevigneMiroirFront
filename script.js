fd09b8d8adf6953eb42ec0894166a005fa07cabb
guillaumesoullard <guillaume.soullard@gmail.com>
2017-04-01 13:19:17 +0200

    debut reactualisation affichage

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
 
     // TODO faire un setInterval pour aller mettre a jour l'affichage tous les X
+
+
+
+
+
+
+
+
     // TODO get screen size and send them in post
 
     //Permet de retrouver la serie active a l'heure de la requete
@@ -22,11 +30,8 @@ $(document).ready(function() {
         return serieActive;
     }();
     //console.log(serieToDisplay);
-    console.log(serieToDisplay.affichages[1].presentation.presentation_duration);
     var dateSerie = new Date(serieToDisplay.affichages[1].presentation.presentation_duration);
-    console.log(dateSerie);
     var dateReference = new Date('1970-01-01T00:00:00+0100');
-    console.log(dateReference);
 
     var diff = dateSerie - dateReference;
 
@@ -34,12 +39,41 @@ $(document).ready(function() {
     var HH = Math.floor(diffSeconds/3600);
     var MM = Math.floor(diffSeconds%3600)/60;
 
-    console.log(diffSeconds);
-    console.log(MM);
-    console.log(HH);
+    //console.log(diffSeconds);
+    /*console.log(MM);
+    console.log(HH);*/
 
     //je veux afficher des presentations
     // il faut que je check tous les x intervalle si je dois changer de presentation en fonction de la duree de chaque presentation
+    var now = new Date();
+    var html = generateHtmlIframe(serieToDisplay.affichages[0]);
+    $('body').append(html);
+
+
+
+    function generateHtmlIframe(affichage)
+    {
+        var iframeHtmlCode = '';
+
+        if(
+            affichage.presentation._id_google_slide != undefined && affichage.presentation._id_google_slide != ''
+            && affichage.presentation.presentation_duration != undefined && affichage.presentation.presentation_duration != ''
+            && affichage.presentation.slider_autostart != undefined && affichage.presentation.slider_autostart != ''
+            && affichage.presentation.slider_loop != undefined && affichage.presentation.slider_loop != ''
+            && affichage.presentation.slide_duration != undefined && affichage.presentation.slide_duration != ''
+        ) {
+            var slider_autostart = 'true';
+            var slider_loop = 'true';
+            (affichage.presentation.slider_autostart == true) ?  slider_autostart = 'true' :  slider_autostart = 'false';
+            (affichage.presentation.slider_loop == true) ?  slider_loop = 'true' :  slider_loop = 'false';
+            iframeHtmlCode = '<iframe src="https://docs.google.com/presentation/d/' + affichage.presentation._id_google_slide + '/embed?start=' + slider_autostart + '&loop=' + slider_loop + '&delayms=' + affichage.presentation.slide_duration.toString() + '" frameborder="0" width="1280px" height="670px" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>';
+        } else {
+            console.log('il manque des infos');
+        }
+        //var iframeHtmlCode = '<iframe src="https://docs.google.com/presentation/d/1oOBuNHorJsZ1AAIWZ4TuqzmzHRWJ38Eh0lAmx5JzNkE/embed?start=true&loop=true&delayms=3000" frameborder="0" width="1280px" height="670px" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>';
+        return iframeHtmlCode;
+
+    }
 
 
 
