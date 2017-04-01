248798a36901ddfbfa53c4542e4960eba67bbe72
guillaumesoullard <guillaume.soullard@gmail.com>
2017-04-01 14:07:15 +0200

    get update serie OK

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 $(document).ready(function() {
 
-    // TODO faire un setInterval pour aller mettre a jour l'affichage tous les X
-
     // il faut que je check tous les x intervalle si je dois changer de presentation en fonction de la duree de chaque presentation
 
     var original = {
         'date': new Date()
     };
 
+    var serieToDisplay = getSerieToDisplay();
+    //console.log(serieToDisplay);
+
     var updated = setInterval(function() {
-        serieToDisplay;
+        serieToDisplay = getSerieToDisplay();
         console.log(serieToDisplay);
-        var html = generateHtmlIframe(serieToDisplay.affichages[0]);
-        $('#iframeDiv').html(html);
-    }, 10000);
+        //je ne met a jour le html que si quelque chose a changer
+        //je regarde le nombre d'affichages et leur dure
+
+        //var nbPresentations = serieToDisplay.affichages.length;
+        //console.log(nbPresentations);
+
+        //pour chaque presentation je fais un set interval sur la duree de la presentation
+        /*serieToDisplay.affichages.forEach(function(affichage) {
+            console.log(affichage);
+        });*/
+        /*var pres = setInterval(function() {
+
+        }, 3000);*/
+
+
+
+        //GENERATE DU CODE HTML
+        //var html = generateHtmlIframe(serieToDisplay.affichages[0]);
+        //$('#iframeDiv').html(html);
+    }, 5000);
 
 
 
@@ -25,7 +43,8 @@ $(document).ready(function() {
     // TODO get screen size and send them in post
 
     //Permet de retrouver la serie active a l'heure de la requete
-    var serieToDisplay = function ()
+    //var serieToDisplay = function ()
+    function getSerieToDisplay()
     {
         var serieActive = {};
         $.ajax({
@@ -34,6 +53,7 @@ $(document).ready(function() {
             dataType: 'json',
             async: false,
             success: function(response) {
+                console.log(response);
                 serieActive = response;
             },
             error: function (response) {
@@ -41,9 +61,10 @@ $(document).ready(function() {
             }
         });
         return serieActive;
-    }();
+    }
+
     //console.log(serieToDisplay);
-    var dateSerie = new Date(serieToDisplay.affichages[1].presentation.presentation_duration);
+    /*var dateSerie = new Date(serieToDisplay.affichages[1].presentation.presentation_duration);
     var dateReference = new Date('1970-01-01T00:00:00+0100');
 
     var diff = dateSerie - dateReference;
@@ -52,14 +73,14 @@ $(document).ready(function() {
     var HH = Math.floor(diffSeconds/3600);
     var MM = Math.floor(diffSeconds%3600)/60;
 
-    //console.log(diffSeconds);
-    /*console.log(MM);
-    console.log(HH);*/
+    console.log(diffSeconds);
+    console.log(MM);
+    console.log(HH);
 
     //je veux afficher des presentations
     var now = new Date();
     var html = generateHtmlIframe(serieToDisplay.affichages[0]);
-    //$('#iframeDiv').html(html);
+    $('#iframeDiv').html(html);*/
 
 
 
